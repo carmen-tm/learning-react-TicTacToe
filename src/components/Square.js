c99968c09999abfcc3ecef50a4dfb010da87558a
carmen-tm <carmentorremolina@gmail.com>
2019-05-11 18:39:06 +0200

    Simplify Square component version with function

--- src/components/Square.js
+++ src/components/Square.js
@@ -1,36 +1,19 @@
-import React, { Component } from 'react';
+import React from 'react';
 import styles from './Square.module.scss';
 
-// function Square(props) {
-// 	// console.log(this.props);
-// 	const { name, newIndex, onClick } = props;
-// 	return (
-// 		<button
-// 			className={styles.square}
-// 			data-id={name}
-// 			data-index={newIndex}
-// 			onClick={onClick}
-// 		>
-// 			{newIndex}
-// 		</button>
-// 	);
-// }
-
-class Square extends Component {
-	render() {
-		// 	// console.log(this.props);
-		const { name, newIndex, onClick } = this.props;
-		return (
-			<button
-				className={styles.square}
-				data-id={name}
-				data-index={newIndex}
-				onClick={onClick}
-			>
-				{newIndex}
-			</button>
-		);
-	}
+function Square(props) {
+	// console.log(this.props);
+	const { name, newIndex, onClick } = props;
+	return (
+		<button
+			className={styles.square}
+			data-id={name}
+			data-index={newIndex}
+			onClick={onClick}
+		>
+			{newIndex}
+		</button>
+	);
 }
 
 export default Square;
