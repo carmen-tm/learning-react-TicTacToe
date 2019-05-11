d197df2e9e10f08637134a8208a2f3ca9fe90eb3
carmen-tm <carmentorremolina@gmail.com>
2019-05-11 19:33:49 +0200

    Add winner message, working just for first row

--- src/components/Board.js
+++ src/components/Board.js
@@ -29,6 +29,23 @@ class Board extends Component {
 		});
 	}
 
+	calculateWinner() {
+		const lines = [[0, 1, 2]];
+		for (let i = 0; i < lines.length; i++) {
+			//Just check winner if there are written things x/o
+			if (this.state.squaresArr[i]) {
+				if (
+					this.state.squaresArr[0] === this.state.squaresArr[1] &&
+					this.state.squaresArr[1] === this.state.squaresArr[2]
+				) {
+					return 'winner!!';
+				} else {
+					return '';
+				}
+			}
+		}
+	}
+
 	//Method of the class Board
 	renderSquare(i) {
 		return (
@@ -40,6 +57,7 @@ class Board extends Component {
 				//Gonna pass a function from mother->childrem to achieve LIFTING (make the childrem by able to change mother's state, and pass new props down)
 				onClick={() => this.handleClick(i)}
 				// onClick={this.handleClick(i)} //No funciona
+				// gameResult={this.calculateWinner()}
 			/>
 		);
 	}
@@ -49,11 +67,19 @@ class Board extends Component {
 	// }
 
 	render() {
-		const status = 'Next player: X';
+		// const status = 'Next player: ' + (this.state.xIsNext ? 'x' : 'o');
+		const { state } = this;
+
+		function status() {
+			const nextLetter = state.xIsNext ? 'x' : 'o';
+			const message = `Next player round: ${nextLetter}`;
+			return message;
+		}
 
 		return (
 			<div>
-				<div className={styles.status}>{status}</div>
+				<h1>The Tic Tac Toe game</h1>
+				<h2 className={styles.status}>{status()}</h2>
 				<div className={styles.boardRow}>
 					{this.renderSquare(0)}
 					{this.renderSquare(1)}
@@ -69,6 +95,7 @@ class Board extends Component {
 					{this.renderSquare(7)}
 					{this.renderSquare(8)}
 				</div>
+				<h3>{this.calculateWinner()}</h3>
 			</div>
 		);
 	}
