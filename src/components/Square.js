c6375a2c854ce3d2f5937267b8ea945acfe730f7
carmen-tm <carmentorremolina@gmail.com>
2019-05-11 12:32:54 +0200

    Start main structure with components etc

---
+++ src/components/Square.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import './Square.scss';
+
+class Square extends Component {
+	render() {
+		return <button className="square">{/* TODO */}</button>;
+	}
+}
+
+export default Square;
